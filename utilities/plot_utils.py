bb8254df3caa29dc13418013ab0050ef01bd76c7
Roy F. Cruz Candelaria <55238184+roy-cruz@users.noreply.github.com>
2023-11-18 00:24:08 -0400

    Updated plot_utils.py

--- utilities/plot_utils.py
+++ utilities/plot_utils.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 
 def heatmaps(dataset, labels, dpi=300):
@@ -39,3 +40,24 @@ def heatmaps(ME, ax=None, title='', x_label='', y_label='', dpi=300):
         fig.set_dpi(dpi)
         fig.set_tight_layout(True)
     return ax
+
+def plotallMEs(eval_recos, evalruns_norm, Xs_train_norm, monitoring_elems, k=0, dpi=200):
+    bins = np.arange(0,62)
+    fig, axes = plt.subplots(4, 4, figsize=(30,30), dpi=dpi)
+    axes = axes.flatten()
+
+    for i, ax in enumerate(axes):
+        reco = eval_recos[k][i]
+        test = evalruns_norm[k][i]
+        for j, histo in enumerate(reco):
+            if j == 0: 
+                ax.step(bins, histo, color='red', alpha=0.8, label='Reco')
+                ax.step(bins, test[j], color='green', alpha=0.3, label='Input')
+            ax.step(bins, histo, color='red', alpha=0.8)
+            ax.step(bins, test[j], color='green', alpha=0.3)
+        ax.step(bins, Xs_train_norm[i].mean(axis=0), color='blue', alpha=1, label='Train')
+        ax.legend(loc='upper right')
+        ax.tick_params(axis='x', labelsize=10)
+        ax.tick_params(axis='y', labelsize=10)
+        ax.set_title(monitoring_elems[i], fontsize=12)   
+    plt.show()
